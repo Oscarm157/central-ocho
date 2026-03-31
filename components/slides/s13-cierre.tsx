de1ae483e9a7b7cf86cf5ac7e57d4faaaa5f5467
Oscarm157 <oscar.amayoral@gmail.com>
2026-03-31 20:40:57 +0000

    S13: fondo video igual que portada

--- components/slides/s13-cierre.tsx
+++ components/slides/s13-cierre.tsx
@@ -1,16 +1,17 @@
-import Image from "next/image";
 import { project, contact } from "@/lib/data";
 
 export function S13Cierre() {
   return (
     <div className="relative w-full h-full overflow-hidden">
-      <Image
-        src="/images/hero-fachada.png"
-        alt="Central Ocho"
-        fill
-        className="object-cover"
-        quality={80}
-      />
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src="/images/hero-video.mp4" type="video/mp4" />
+      </video>
       <div className="absolute inset-0 bg-black/70" />
 
       <div className="absolute inset-0 flex flex-col items-center justify-center px-6 text-center">
